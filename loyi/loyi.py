ea9f1bc9d7b976bf05fbe1a38180bb1cfe847d26
ethanmiles <percivalstr@163.com>
2019-04-09 00:43:33 +0800

    loyi

---
+++ loyi/loyi.py
@@ -0,0 +1,36 @@
+import h5py
+import numpy as np
+import collections
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+path = 'newratinglist20.mat'
+np.set_printoptions(precision=0, suppress=True)
+
+
+class Analysis:
+    def __init__(self, path):
+        f = h5py.File(path, "r")
+        name = re.findall(r"'(.*)'", str(f.keys()))[0]
+        mat_t = f[name]
+        mat = np.transpose(mat_t)
+        mat = mat.astype(np.int32)  # float2int
+        mat[:, 2] = mat[:, 2] / 24  # hours2day
+        self.mat = mat
+
+    def 打印A的频数(self, a):
+        col = self.mat[:, a]
+        plt.hist(col, bins=1000, normed=0, facecolor="blue",
+                 edgecolor="black", alpha=0.7)
+        plt.show()
+
+    def 打印A列等于B时C列的频数(self, a, b, c):
+        col = self.mat[self.mat[:, a] == b][:, c]
+        plt.hist(col, bins=1000, normed=0, facecolor="blue",
+                 edgecolor="black", alpha=0.7)
+        plt.show()
+
+
+if __name__ == "__main__":
+    als = Analysis('newratinglist20.mat')
+    als.打印A列等于B时C列的频数(1, 1, 2)
