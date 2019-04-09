50fe5735f60aabed304c589710d11f71f5e627e8
ethanmiles <percivalstr@163.com>
2019-04-09 13:44:25 +0800

    loyi

--- loyi/loyi.py
+++ loyi/loyi.py
@@ -15,7 +15,7 @@ class Analysis:
         mat_t = f[name]
         mat = np.transpose(mat_t)
         mat = mat.astype(np.int32)  # float2int
-        mat[:, 2] = mat[:, 2] / 24  # hours2day
+        mat[:, 2] = mat[:, 2] / (24*30)   # hours2day
         self.mat = mat
 
     def 打印A的频数(self, a):
@@ -26,9 +26,23 @@ class Analysis:
 
     def 打印A列等于B时C列的频数(self, a, b, c):
         col = self.mat[self.mat[:, a] == b][:, c]
-        plt.hist(col, bins=1000, normed=0, facecolor="blue",
+        plt.hist(col, bins=25, normed=0, facecolor="blue",
                  edgecolor="black", alpha=0.7)
         plt.show()
+
+    def A列分位数(self, a):
+        col = self.mat[:, a]
+        a = round(np.mean(col), 2)
+        b = np.median(col)
+        c = pd.DataFrame(col).describe()
+        print(c)
+
+    def 打印A列等于B时C列的分位数(self, a, b, c):
+        col = self.mat[self.mat[:, a] == b][:, c]
+        a = round(np.mean(col), 2)
+        b = np.median(col)
+        c = pd.DataFrame(col).describe()
+        print(c)
 
 
 if __name__ == "__main__":
