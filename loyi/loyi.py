0a641894b316857a74662bd66e45451f6b670980
ethanmiles <percivalstr@163.com>
2019-04-09 14:12:34 +0800

    loyi

--- loyi/loyi.py
+++ loyi/loyi.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import re
 import seaborn as sb
+
+# file path config
 path = 'newratinglist20.mat'
+
+# numpy global config
 np.set_printoptions(precision=0, suppress=True)
+
+# seaborn global config
 sb.set()
+sb.set_style('darkgrid')
 
 
 class Analysis:
@@ -20,37 +27,41 @@ class Analysis:
         mat[:, 2] = mat[:, 2] / (24*30)   # hours2month
         self.mat = mat
 
+    def get_frequency_of_A(self, a):
+        return self.mat[:, a]
+
+    def get_frequency_of_colC_where_colA_is_B(self, a, b, c):
+        return self.mat[self.mat[:, a] == b][:, c]
+
     def paint_frequency_of_A_hist(self, a):
-        col = self.mat[:, a]
+        col = self.get_frequency_of_A(a)
         plt.hist(col, bins=1000, normed=0, facecolor="blue",
                  edgecolor="black", alpha=0.7)
         plt.show()
 
     def paint_colC_when_colA_is_B_hist(self, a, b, c):
-        col = self.mat[self.mat[:, a] == b][:, c]
+        col = self.get_frequency_of_colC_where_colA_is_B(a, b, c)
         plt.hist(col, bins=25, normed=0, facecolor="blue",
                  edgecolor="black", alpha=0.7)
         plt.show()
 
     def print_colA_Quantile(self, a):
-        col = self.mat[:, a]
+        col = self.get_frequency_of_A(a)
         c = pd.DataFrame(col).describe()
         print(c)
 
     def print_colC_when_colA_is_B_Quantile(self, a, b, c):
-        col = self.mat[self.mat[:, a] == b][:, c]
+        col = self.get_frequency_of_colC_where_colA_is_B(a, b, c)
         c = pd.DataFrame(col).describe()
         print(c)
 
     def sbpaint_colC_when_colA_is_B_kde(self, a, b, c):
-        sb.set_style('darkgrid')
-        col = self.mat[self.mat[:, a] == b][:, c]
+        col = self.get_frequency_of_colC_where_colA_is_B(a, b, c)
         sb.distplot(col, kde_kws={"label": "KDE"}, color="y")
         plt.show()
 
     def sbpaint_frequency_of_A_kde(self, a):
-        sb.set_style('darkgrid')
-        col = self.mat[:, a]
+        col = self.get_frequency_of_A(a)
         sb.distplot(col, kde_kws={"label": "KDE"}, color="y")
         plt.show()
 
